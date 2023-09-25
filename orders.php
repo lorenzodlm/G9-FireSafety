2321f13305d8cedee9e6c19e299e41068518b8ca
lorenzodlm <145830734+lorenzodlm@users.noreply.github.com>
2023-09-25 23:21:26 +0700

    Update orders.php

--- orders.php
+++ orders.php
@@ -1,5 +1,21 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Check if the user is logged in
+if (!isset($_SESSION['userId']) || !isset($_SESSION['userEmail'])) {
+    header('Location: login.php'); // Redirect to login page if not logged in
+    exit;
+}
+
+include 'dbconnect.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,11 +27,15 @@
             margin: 0;
             padding: 0;
             background-color: #f4f4f4;
-            display: flex;           /* Make body a flex container */
-            flex-direction: column;  /* Stack children vertically */
-            justify-content: center; /* Center children vertically */
-            align-items: center;     /* Center children horizontally */
-            height: 100vh;           
+            display: flex;
+            /* Make body a flex container */
+            flex-direction: column;
+            /* Stack children vertically */
+            justify-content: center;
+            /* Center children vertically */
+            align-items: center;
+            /* Center children horizontally */
+            height: 100vh;
         }
 
         .big-container {
@@ -28,7 +48,7 @@
             position: fixed;
             top: 0;
             left: 0;
-            
+
         }
 
         .container {
@@ -80,11 +100,14 @@
         nav a:hover {
             background-color: #ffffff;
         }
-        
+
         .top-background {
-            background: url('topbg.jpg') no-repeat center center; /* Replace with your image path */
-            background-size: cover; /* This will cover the entire viewport */
-            height: 650px; /* Adjust based on your needs */
+            background: url('topbg.jpg') no-repeat center center;
+            /* Replace with your image path */
+            background-size: cover;
+            /* This will cover the entire viewport */
+            height: 650px;
+            /* Adjust based on your needs */
             width: 100%;
         }
 
@@ -108,17 +131,21 @@
             width: 100%;
             z-index: 1000;
         }
+
         main {
             max-width: 800px;
             margin: 0 auto;
             padding: 20px;
         }
+
         table {
             width: 100%;
             border-collapse: collapse;
             margin-top: 20px;
         }
-        table th, table td {
+
+        table th,
+        table td {
             border: 1px solid #ddd;
             padding: 8px;
             text-align: left;
@@ -128,19 +155,19 @@
         table th {
             background-color: #f2f2f2;
         }
-
     </style>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customer Orders</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Customer Orders</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
 
 <body>
-<header>
+    <header>
         <h1>FireSafety</h1>
         <nav>
             <a href="index.php">Home</a>
@@ -161,13 +188,118 @@
         </nav>
     </header>
     <main>
-        <div class="order-list">
+        <div class="container">
             <?php
-            
-            
+
+            // Table for Customers Orders
+            // Check if the user is an employee or technician
+            if ($_SESSION['userType'] == 'customer' || $_SESSION['userType'] == 'businesscustomer') {
+                $currentUserId = $_SESSION['userId']; // Assuming you store logged-in user's ID in $_SESSION['userId']
+
+                $sql = "SELECT customer.c_id, customer.c_firstName, customer.c_lastName, customer.c_email, orders.order_id, item.item_name, item.item_id, order_items.quantity, item.item_price 
+                    FROM customer
+                    JOIN orders ON customer.c_id = orders.c_id
+                    JOIN order_items ON orders.order_id = order_items.order_id
+                    JOIN item ON order_items.item_id = item.item_id
+                    WHERE customer.c_id = $currentUserId"; // Filter results for the current user
+
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    echo "<h2>Customers Orders</h2>";
+                    echo "<table border='1'>";
+                    echo "<tr>";
+                    echo "<th>Customer ID</th>";
+                    echo "<th>First Name</th>";
+                    echo "<th>Last Name</th>";
+                    echo "<th>Email</th>";
+                    echo "<th>Order ID</th>";
+                    echo "<th>Item Name</th>";
+                    echo "<th>Item ID</th>";
+                    echo "<th>Quantity</th>";
+                    echo "<th>Price THB</th>";
+                    echo "<th>Total Price THB</th>";
+                    echo "</tr>";
+
+                    // Fetch and display records from the joined tables
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>{$row['c_id']}</td>";
+                        echo "<td>{$row['c_firstName']}</td>";
+                        echo "<td>{$row['c_lastName']}</td>";
+                        echo "<td>{$row['c_email']}</td>";
+                        echo "<td>{$row['order_id']}</td>";
+                        echo "<td>{$row['item_name']}</td>";
+                        echo "<td>{$row['item_id']}</td>";
+                        echo "<td>{$row['quantity']}</td>";
+                        echo "<td>{$row['item_price']}</td>";
+                        $totalPrice = $row['quantity'] * $row['item_price']; // Calculate Total Price
+                        echo "<td>{$totalPrice}</td>"; // Display Total Price
+                        echo "</tr>";
+                    }
+
+                    echo "</table>";
+                } else {
+                    echo "0 results";
+                }
+            }
+
+            // Table for Check Up
+            // Check if the user is an employee or technician
+            if ($_SESSION['userType'] == 'customer' || $_SESSION['userType'] == 'businesscustomer') {
+                $currentUserId = $_SESSION['userId']; // Assuming you store logged-in user's ID in $_SESSION['userId']
+
+                $sql = "SELECT checkup.checkup_id, employee.e_firstName, employee.e_lastName, item.item_name, checkup.check_date, checkup.check_time, customer.c_id, customer.c_address, checkup.check_status
+                FROM checkup
+                JOIN employee ON checkup.e_id = employee.e_id
+                JOIN customer ON checkup.c_id = customer.c_id
+                JOIN item ON checkup.item_id = item.item_id
+                WHERE customer.c_id = $currentUserId"; // Filter results for the current user
+
+
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    echo "<h2>Check Ups</h2>";
+                    echo "<table border='1'>";
+                    echo "<tr>";
+                    echo "<th>Checkup ID</th>";
+                    echo "<th>Employee First Name</th>";
+                    echo "<th>Employee Last Name</th>";
+                    echo "<th>Item Name</th>";
+                    echo "<th>Check Date</th>";
+                    echo "<th>Check Time</th>";
+                    echo "<th>Customer ID</th>";
+                    echo "<th>Customer Address</th>";
+                    echo "<th>Check Status</th>";
+                    echo "</tr>";
+
+                    // Fetch and display records from the joined tables
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>{$row['checkup_id']}</td>";
+                        echo "<td>{$row['e_firstName']}</td>";
+                        echo "<td>{$row['e_lastName']}</td>";
+                        echo "<td>{$row['item_name']}</td>";
+                        echo "<td>{$row['check_date']}</td>";
+                        echo "<td>{$row['check_time']}</td>";
+                        echo "<td>{$row['c_id']}</td>";
+                        echo "<td>{$row['c_address']}</td>";
+                        echo "<td>{$row['check_status']}</td>";
+                        echo "</tr>";
+                    }
+
+                    echo "</table>";
+                } else {
+                    echo "0 results";
+                }
+            }
 
             ?>
         </div>
     </main>
 </body>
+
 </html>
+
+<?php $conn->close() ?>
